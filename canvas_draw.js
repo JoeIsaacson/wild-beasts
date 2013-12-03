30b94cc8a7fc3ed732bdf05fa35e70c2165d0b33
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-03 18:41:02 -0500

    animation modified

--- canvas_draw.js
+++ canvas_draw.js
@@ -17,20 +17,22 @@ function showVideo(){
     easing: jswing
   });*/
 
-  $(".top-images .one").animate({top : '-=1000px',}, 600, 'swing');
-  $(".top-images .two").animate({top : '-=1000px',}, 900, 'swing');
-  $(".top-images .three").animate({top : '-=1000px',}, 1500, 'swing');
-  $(".top-images .four").animate({top : '-=1000px',}, 2400, 'swing');
-  $(".top-images .five").animate({top : '-=1000px',}, 3600, 'swing');
-  $(".top-images .six").animate({top : '-=1000px',}, 4500, 'easeOutSine');
+  $(".top-images .one").animate({top : '-=1000px',}, 900, 'easeInBack');
+  $(".top-images .two").animate({top : '-=1000px',}, 1000, 'easeInBack');
+  $(".top-images .three").animate({top : '-=1000px',}, 1100, 'easeInBack');
+  $(".top-images .four").animate({top : '-=1000px',}, 1200, 'easeInBack');
+  $(".top-images .five").animate({top : '-=1000px',}, 1300, 'easeInBack');
+  $(".top-images .six").animate({top : '-=1000px',}, 1400, 'easeInBack');
 
-  $(".bottom-images .one").animate({top : '+=1000px',}, 5600, 'linear');
-  $(".bottom-images .two").animate({top : '+=1000px',}, 4500, 'linear');
-  $(".bottom-images .three").animate({top : '+=1000px',}, 3600, 'linear');
-  $(".bottom-images .four").animate({top : '+=1000px',}, 2400, 'linear');
-  $(".bottom-images .five").animate({top : '+=1000px',}, 1500, 'linear');
-  $(".bottom-images .six").animate({top : '+=1000px',}, 900, 'linear');
-  $(".bottom-images .seven").animate({top : '+=1000px',}, 600, 'linear');
+  $(".bottom-images .one").animate({top : '+=1000px',}, 1500, 'easeInBack');
+  $(".bottom-images .two").animate({top : '+=1000px',}, 1400, 'easeInBack');
+  $(".bottom-images .three").animate({top : '+=1000px',}, 1300, 'easeInBack');
+  $(".bottom-images .four").animate({top : '+=1000px',}, 1200, 'easeInBack');
+  $(".bottom-images .five").animate({top : '+=1000px',}, 1100, 'easeInBack');
+  $(".bottom-images .six").animate({top : '+=1000px',}, 1000, 'easeInBack');
+  $(".bottom-images .seven").animate({top : '+=1000px',}, 900, 'easeInBack');
+  $(".bottom-images .eight").animate({top : '+=1000px',}, 800, 'easeInBack');
+
 
   $('#show-video').hide();
 }
