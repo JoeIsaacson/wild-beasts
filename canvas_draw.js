284b34a7b67934004efea6c6cd7b06c49aad995c
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-03 16:54:57 -0500

    updated timing

--- canvas_draw.js
+++ canvas_draw.js
@@ -12,23 +12,20 @@ function move_lower_triangles(){
 function showVideo(){
   console.log('buttonpressed');
 
-  $(".top-images .one").animate({marginTop : '-=1000px',}, { duration: 800 });
-  $(".top-images .two").animate({marginTop : '-=1000px',}, { duration: 1400 });
-  $(".top-images .three").animate({marginTop : '-=1000px',}, { duration: 2600 });
-  $(".top-images .four").animate({marginTop : '-=1000px',}, { duration: 4200 });
-  $(".top-images .five").animate({marginTop : '-=1000px',}, { duration: 7800 });
-  $(".top-images .six").animate({marginTop : '-=1000px',}, { duration: 9200 });
+  $(".top-images .one").animate({marginTop : '-=1000px',}, { duration: 600});
+  $(".top-images .two").animate({marginTop : '-=1000px',}, { duration: 900 });
+  $(".top-images .three").animate({marginTop : '-=1000px',}, { duration: 1500 });
+  $(".top-images .four").animate({marginTop : '-=1000px',}, { duration: 2400 });
+  $(".top-images .five").animate({marginTop : '-=1000px',}, { duration: 3600 });
+  $(".top-images .six").animate({marginTop : '-=1000px',}, { duration:  5100 });
 
-
-  $(".bottom-images .one").animate({marginTop : '+=1000px',}, { duration: 1200 });
+  $(".bottom-images .one").animate({marginTop : '+=1000px',}, { duration: 1 });
   $(".bottom-images .two").animate({marginTop : '+=1000px',}, { duration: 9200 });
   $(".bottom-images .three").animate({marginTop : '+=1000px',}, { duration: 7800 });
   $(".bottom-images .four").animate({marginTop : '+=1000px',}, { duration: 4200 });
   $(".bottom-images .five").animate({marginTop : '+=1000px',}, { duration: 2600 });
   $(".bottom-images .six").animate({marginTop : '+=1000px',}, { duration: 1400 });
   $(".bottom-images .seven").animate({marginTop : '+=1000px',}, { duration: 800 });
-
-
 
   $('#show-video').hide();
 }
