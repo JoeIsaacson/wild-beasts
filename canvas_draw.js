ade3cb32a9d78bcbf3917dae2c1cdf591af1f77c
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-03 17:01:58 -0500

    added new animatiom

--- canvas_draw.js
+++ canvas_draw.js
@@ -17,6 +17,33 @@ function showVideo(){
   $('.lower-poly').animate({
     top : '+=1000px',
   }, { duration: 8000, queue: false });
+}
+
+$(window).on('scroll', function(){
+  move_lower_triangles();
+});
+
+
+
+
+function showVideo(){
+  console.log('buttonpressed');
+
+  $(".upper-poly.one").animate({top : '-=1000px',}, { duration: 600});
+  $(".upper-poly.two").animate({top : '-=1000px',}, { duration: 900 });
+  $(".upper-poly.three").animate({top : '-=1000px',}, { duration: 1500 });
+  $(".upper-poly.four").animate({top : '-=1000px',}, { duration: 2400 });
+  $(".upper-poly.five").animate({top : '-=1000px',}, { duration: 3600 });
+
+  $(".lower-poly.seven").animate({top : '+=1000px',}, { duration: 3600 });
+  $(".lower-poly.eight").animate({top : '+=1000px',}, { duration: 2400 });
+  $(".lower-poly.nine").animate({top : '+=1000px',}, { duration: 1500 });
+  $(".lower-poly.ten").animate({top : '+=1000px',}, { duration: 900 });
+  $(".lower-poly.eleven").animate({top : '+=1000px',}, { duration: 600 });
+  $(".lower-poly.twelve").animate({top : '+=1000px',}, { duration: 300 });
+
   $('#show-video').hide();
 }
+
+
 
