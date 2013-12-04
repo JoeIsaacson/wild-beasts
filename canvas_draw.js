3bcb298eb4b6e2e6215768b7a70e41b4a6f10559
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-04 15:19:49 -0500

    layout mastered, jquery animation needs work

--- canvas_draw.js
+++ canvas_draw.js
@@ -1,38 +1,33 @@
-$(window).on('scroll', function(){
-  move_lower_triangles();
-});
 
+$(document).ready(function(){
+      /*QUAD 1 ANIMATIONS*/
 
-function move_lower_triangles(){
-  var scrollPosition = window.scrollY;
-}
+      $(".quad-1")
+        .delay(2000)
+        .queue( function(one){
+        $(this).css('background', 'url("img/p-2.png") no-repeat scroll right bottom/cover transparent'); 
+        one();
+      });
+      
+      $(".quad-1")
+        .delay(2000)
+        .queue( function(one){
+        $(this).css('background', 'url("img/p-3.png") no-repeat scroll right bottom/cover transparent'); 
+        one();
+      });
 
-$(document).scroll(function(){
-      var scrollY = $(window).scrollTop();
-      console.log('scroll height =', scrollY);
-      $(".top-images .one").css('top', 'scrollY * 100');
-      console.log('function ran bro');
-});
+      $(".quad-1")
+        .delay(2000)
+        .queue( function(one){
+        $(this).css('background', 'url("img/p-4.png") no-repeat scroll right bottom/cover transparent'); 
+        one();
+      });  
 
-/*function showVideo(){
-  console.log('buttonpressed');
-  $(".top-images .one").animate({top : '-=1000',}, 900, 'easeInBack');
-  $(".top-images .two").animate({top : '-=1000px',}, 1000, 'easeInBack');
-  $(".top-images .three").animate({top : '-=1000px',}, 1100, 'easeInBack');
-  $(".top-images .four").animate({top : '-=1000px',}, 1200, 'easeInBack');
-  $(".top-images .five").animate({top : '-=1000px',}, 1300, 'easeInBack');
-  $(".top-images .six").animate({top : '-=1000px',}, 1400, 'easeInBack');
-
-  $(".bottom-images .one").animate({top : '+=1000px',}, 1500, 'easeInBack');
-  $(".bottom-images .two").animate({top : '+=1000px',}, 1400, 'easeInBack');
-  $(".bottom-images .three").animate({top : '+=1000px',}, 1300, 'easeInBack');
-  $(".bottom-images .four").animate({top : '+=1000px',}, 1200, 'easeInBack');
-  $(".bottom-images .five").animate({top : '+=1000px',}, 1100, 'easeInBack');
-  $(".bottom-images .six").animate({top : '+=1000px',}, 1000, 'easeInBack');
-  $(".bottom-images .seven").animate({top : '+=1000px',}, 900, 'easeInBack');
-  $(".bottom-images .eight").animate({top : '+=1000px',}, 800, 'easeInBack');
-
-  //HIDE BUTTON AFTER FUNCTION
-  $('#show-video').hide();
-}*/
+      $(".quad-1")
+        .delay(2000)
+        .queue( function(one){
+        $(this).css('background', 'none'); 
+        one();
+      });     
+});  
 
