f59aa00d8d254b134748050ba06422bf2ea1effc
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-04 17:24:10 -0500

    all images added

--- canvas_draw.js
+++ canvas_draw.js
@@ -2,8 +2,9 @@
 $(window).bind("load", function() {      
 
       /*QUAD 1 ANIMATIONS*/
+      var speed = 200;
 
-      $(".quad-1 .one").delay(400).hide(0);
+      $(".quad-1 .one").delay(600).hide(0);
       console.log("hidden")
 
       $(".quad-1 .two").delay(800).hide(0);
@@ -27,6 +28,34 @@ $(window).bind("load", function() {
       console.log("hidden 3")
 
       $(".quad-2 .four").delay(3200).hide(0);
+      console.log("hidden 4")
+
+      /*QUAD 3 ANIMATIONS*/
+
+      $(".quad-3 .one").delay(3600).hide(0);
+      console.log("hidden")
+
+      $(".quad-3 .two").delay(4000).hide(0);
+      console.log("hidden 2")
+
+      $(".quad-3 .three").delay(4400).hide(0);
+      console.log("hidden 3")
+
+      $(".quad-3 .four").delay(4800).hide(0);
+      console.log("hidden 4")
+
+      /*QUAD 4 ANIMATIONS*/
+
+      $(".quad-4 .one").delay(5200).hide(0);
+      console.log("hidden")
+
+      $(".quad-4 .two").delay(5600).hide(0);
+      console.log("hidden 2")
+
+      $(".quad-4 .three").delay(6000).hide(0);
+      console.log("hidden 3")
+
+      $(".quad-4 .four").delay(6400).hide(0);
       console.log("hidden 4")
 });  
 
