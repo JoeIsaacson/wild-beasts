95c0a3b930108be22fe9ff315d5a67846240c4af
Joey Isaacson <joeisaacson23@gmail.com>
2013-12-04 16:16:37 -0500

    new images added

--- canvas_draw.js
+++ canvas_draw.js
@@ -1,33 +1,32 @@
 
-$(document).ready(function(){
+$(window).bind("load", function() {      
+
       /*QUAD 1 ANIMATIONS*/
 
-      $(".quad-1")
-        .delay(2000)
-        .queue( function(one){
-        $(this).css('background', 'url("img/p-2.png") no-repeat scroll right bottom/cover transparent'); 
-        one();
-      });
-      
-      $(".quad-1")
-        .delay(2000)
-        .queue( function(one){
-        $(this).css('background', 'url("img/p-3.png") no-repeat scroll right bottom/cover transparent'); 
-        one();
-      });
+      $(".quad-1 .one").delay(400).hide(0);
+      console.log("hidden")
 
-      $(".quad-1")
-        .delay(2000)
-        .queue( function(one){
-        $(this).css('background', 'url("img/p-4.png") no-repeat scroll right bottom/cover transparent'); 
-        one();
-      });  
+      $(".quad-1 .two").delay(800).hide(0);
+      console.log("hidden 2")
 
-      $(".quad-1")
-        .delay(2000)
-        .queue( function(one){
-        $(this).css('background', 'none'); 
-        one();
-      });     
+      $(".quad-1 .three").delay(1200).hide(0);
+      console.log("hidden 3")
+
+      $(".quad-1 .four").delay(1600).hide(0);
+      console.log("hidden 4")
+
+      /*QUAD 2 ANIMATIONS*/
+
+      $(".quad-2 .one").delay(2000).hide(0);
+      console.log("hidden")
+
+      $(".quad-2 .two").delay(2400).hide(0);
+      console.log("hidden 2")
+
+      $(".quad-2 .three").delay(2800).hide(0);
+      console.log("hidden 3")
+
+      $(".quad-2 .four").delay(3200).hide(0);
+      console.log("hidden 4")
 });  
 
